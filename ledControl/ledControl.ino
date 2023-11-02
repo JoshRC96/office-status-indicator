9db9b4ebcecb72516619a8f0821f2c14281960fa
Joshuel Rodriguez <joshuelrodriguezc96@gmail.com>
2023-11-01 20:44:49 -0400

    Created initial Arduino sketch
    Arduino sketch was created. This sketch will be responsible for
    processing the information sent by the Python script and communicating
    directly with the Arduino board and ultimately controling the RGB LED.
    
    This initial sketch defined the redPin, bluePin, and greenPin of the LED
    and set the pinMode of each to OUTPUT. These are pins 11, 10, and 9,
    respectively.

---
+++ ledControl/ledControl.ino
@@ -0,0 +1,16 @@
+//Defining each RGB pin with the board
+int redPin = 11;
+int bluePin = 10;
+int greenPin = 9;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
