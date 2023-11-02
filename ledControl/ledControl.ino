9c572b7f7e11bd192969b95426222189147ceb1a
Joshuel Rodriguez <joshuelrodriguezc96@gmail.com>
2023-11-01 21:17:23 -0400

    Add serial port comm and defined officeStatus var.
    Added serial port communication in void setup() to enable communication
    with the Python script.
    
    Added if conditional to check if information was recieved through the
    serial port.
    
    Defined the string variable officeStatus that will receive its value
    from the Python script and will determine the color of the LED.
    
    Set if statements to check the value of officeStatus to set the color
    of the LED appropriately. The LED will turn red for officeStatus = "busy",
    white for officeStatus = "not busy", and will turn off for "off".

--- ledControl/ledControl.ino
+++ ledControl/ledControl.ino
@@ -5,6 +5,7 @@ int greenPin = 9;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); //Setting serial communication to communicate with the Python script
   pinMode(redPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -12,5 +13,20 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  if (Serial.available() > 0) {
+    String officeStatus = Serial.read(); //read variable value from Python script through the serial port and will be "busy", "not bussy", or "off"
+    if (officeStatus == "busy") {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(greenPin, LOW);
+    } else if (officeStatus == "not busy") {
+      digitalWrite(redPin, HIGH);
+      digitalWrite(bluePin, HIGH);
+      digitalWrite(greenPin, HIGH);
+    } else {
+      digitalWrite(redPin, LOW);
+      digitalWrite(bluePin, LOW);
+      digitalWrite(greenPin, LOW);
+    }
+  }
 }
